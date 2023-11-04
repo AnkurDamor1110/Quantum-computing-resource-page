124f842b2cd6a788cd7b4c4874e59b892cd5a552
Dhruvin2910 <dhruvinpatel321@gmail.com>
2023-11-04 14:50:46 +0530

    Json file of Tutorials added

--- src/Data/Tutorial/Courses_data.js
+++ src/Data/Tutorial/Courses_data.js
@@ -1,19 +1,66 @@
 // data.js
 const CoursesData =[
-    {
-    title: '',
-    author: '',
-    link: '', 
+  {
+    'Title ': 'Introduction to Quantum Information',
+    'Author Name': 'Joonwoo Bae',
+    URL: 'https://www.coursera.org/learn/introduction-to-quantum-information'
   },
   {
-    title: '',
-    author: '',
-    link: '', 
+    'Title ': 'Quantum Optics 2 - Two photons and more',
+    'Author Name': 'Alain Aspect and',
+    URL: 'https://www.coursera.org/learn/quantum-optics-two-photons'
+  },
+  { 'Author Name': 'Michel Brune' },
+  {
+    'Title ': 'Understanding Modern Physics II: Quantum Mechanics and Atoms',
+    'Author Name': 'Yi Wang',
+    URL: 'https://www.coursera.org/learn/understanding-modern-physics-2-quantum-mechanics-and-atoms'
   },
   {
-    title: '',
-    author: '',
-    link: '',
+    'Title ': 'Introduction to Quantum Computing for Everyone',
+    'Author Name': 'UChicagoX',
+    URL: 'https://www.edx.org/learn/quantum-computing/university-of-chicago-introduction-to-quantum-computing-for-everyone?index=product&objectID=course-e08ab5d6-00e1-4cab-8e00-12d05299e906&webview=false&campaign=Introduction+to+Quantum+Computing+for+Everyone&source=edX&product_category=course&placement_url=https%3A%2F%2Fwww.edx.org%2Flearn%2Fquantum-computing'   
+  },
+  {
+    'Title ': 'THE QUANTUM INTERNET AND QUANTUM COMPUTERS: HOW WILL THEY CHANGE THE WORLD? (QUANTUM INFORMATIONAL COURSE)',
+    'Author Name': 'Stephanie Wehner, Lieven Vandersypen and Menno Veldhorst',
+    URL: 'https://www.edx.org/learn/science/delft-university-of-technology-the-quantum-internet-and-quantum-computers-how-w-2'
+  },
+  {
+    'Title ': 'FUTURE LEARN',
+    'Author Name': 'Takahiko Satoh',
+    URL: 'https://www.futurelearn.com/courses/intro-to-quantum-computing'
+  },
+  { 'Author Name': 'Rodney Van Meter' },
+  {
+    'Title ': 'GEEKS FOR GEEKS',
+    'Author Name': 'geeksforgeeks',
+    URL: 'https://www.geeksforgeeks.org/introduction-quantum-computing/'
+  },
+  {
+    'Title ': 'MICROSOFT’S QUANTUM COMPUTING FOR COMPUTER SCIENTISTS (PDF COURSE)',
+    'Author Name': 'microsoft',
+    URL: 'https://www.youtube.com/watch?v=F_Riqjdh2oM'
+  },
+  {
+    'Title ': 'UDEMY QUANTUM COMPUTING — AN OVERVIEW',
+    'Author Name': 'Lakshmy Subramonia Iyer',
+    URL: 'https://www.udemy.com/course/quantum-computing-an-overview/'
+  },
+  {
+    'Title ': 'Introduction to Quantum Computing',
+    'Author Name': 'UBCx',
+    URL: 'https://www.edx.org/learn/science/university-of-british-columbia-intro-to-quantum-computing?index=product&objectID=course-8d965135-2d45-484c-9987-1ffabc4074b5&webview=false&campaign=Introduction+to+Quantum+Computing&source=edX&product_category=course&placement_url=https%3A%2F%2Fwww.edx.org%2Flearn%2Fquantum-computing'
+  },
+  {
+    'Title ': 'Quantum Computation Roadmap',
+    'Author Name': 'Richard Hughes,Malcolm Boshier.',
+    URL: 'https://qist.lanl.gov/qcomp_map.shtml'
+  },
+  {
+    'Title ': 'Concepts of Quantum Computing',
+    'Author Name': 'Nikita Duggal',
+    URL: 'https://www.simplilearn.com/tutorials/machine-learning-tutorial/what-is-quantum-computing#:~:text=Quantum%20computing%20is%20a%20branch,1%20at%20the%20same%20time.'
   },
 ];
   
---
+++ src/Data/Tutorial/Youtube_data.js
@@ -0,0 +1,188 @@
+const YoutubeData = [
+  {
+      type: 'Videos',
+      content: [
+        {
+          'Title ': 'How Does a Quantum Computer Work?',
+          'channel ': 'Veritasium',
+          URL: 'https://youtu.be/g_IaVepNDT4'
+        },
+        {
+          'Title ': 'Quantum Computing for Computer Scientists',
+          'channel ': 'Microsoft Research',
+          URL: 'https://youtube.com/playlist?list=PLD7HFcN7LXRcKEiyRxdTDVRaMZQDhojhb&feature=shared'
+        },
+        {
+          'Title ': 'Qubits and Quantum States, Quantum Circuits, Measurements',
+          'channel ': 'Qiskit',
+          URL: 'https://youtube.com/playlist?list=PLOFEBzvs-VvrXTMy5Y2IqmSaUjfnhvBHR&feature=shared'
+        },
+        {
+          'Title ': 'What is  quantum computing?',
+          'channel ': 'Drishti IAS:English',
+          URL: 'https://youtu.be/h3w_B7BN_Qw'
+        },
+        {
+          'Title ': 'Quantum Computing: Algorithm, Programming and Hardware, an Introduction',
+          'channel ': 'Introduction to Quantum computing',
+          URL: 'https://youtube.com/playlist?list=PLnK6MrIqGXsJfcBdppW3CKJ858zR8P4eP&feature=shared'
+        },
+        {
+          'Title ': 'Overview',
+          'channel ': 'Sandro Mareco',
+          URL: 'https://youtube.com/playlist?list=PL74Rel4IAsETUwZS_Se_P-fSEyEVQwni7&feature=shared'
+        },
+        {
+          'Title ': 'Introduction : Motivation & Overview ',
+          'channel ': 'Quantumn computing',
+          URL: 'https://youtube.com/playlist?list=PLo4DhXMUkdvU9rZvEQYLdly5dABHvlZuD&feature=shared'
+        },
+        {
+          'Title ': 'Quantum Computation and Quantum Information',
+          'channel ': 'Prabha Mandayam',
+          URL: 'https://youtube.com/playlist?list=PLqLyTdPNhQZwfLoL4QMeI6scnyH1c__tE&feature=shared'
+        },
+        {
+          'Title ': 'QC Theory',
+          'channel ': 'QCTheory',
+          URL: 'https://youtube.com/playlist?list=PLQPy6m18sNVSNTP96XzTveWBMOxy9gZbN&feature=shared'
+        },
+        {
+          'Title ': 'The qubit',
+          'channel ': 'Michael Nielsen',
+          URL: 'https://youtube.com/playlist?list=PL1826E60FD05B44E4&feature=shared'
+        },
+        {
+          'Title ': 'Double slit experiment',
+          'channel ': 'Aritra Sarkar',
+          URL: 'https://youtube.com/playlist?list=PLnhoxwUZN7-6hB2iWNhLrakuODLaxPTOG&feature=shared'
+        },
+        {
+          'Title ': 'Introduction to quantum computing course 2022',
+          'channel ': 'NYU Quantum Technology Lab',
+          URL: 'https://youtube.com/playlist?list=PLo0Vs5tDeRLRIPcJ83SN91M-asGuaa1AD&feature=shared'
+        },
+        {
+          'Title ': 'Quantum Computing',
+          'channel ': 'Microsoft Research',
+          URL: 'https://www.youtube.com/playlist?list=PLD7HFcN7LXRcKEiyRxdTDVRaMZQDhojhb'
+        }
+      ]
+  },
+  {
+      type: 'Channels',
+      content: [
+        {
+          'Youtbe Channel': 'Quantum Computing',
+          URL: 'https://www.youtube.com/@quantumcomputing2985/featured'
+        },
+        {
+          'Youtbe Channel': 'Introduction to Quantum Computing',
+          URL: 'https://www.youtube.com/channel/UCRBLbygoPJ4wuM5HvNLmnSQ'
+        },
+        {
+          'Youtbe Channel': 'Institute for Quantum Computing',
+          URL: 'https://www.youtube.com/user/QuantumIQC'
+        },
+        {
+          'Youtbe Channel': 'Qiskit ',
+          URL: 'https://www.youtube.com/channel/UClBNq7mCMf5xm8baE_VMl3A'
+        },
+        {
+          'Youtbe Channel': 'Quantum Computing Inc',
+          URL: 'https://www.youtube.com/channel/UC8rBucCY0objG4Aeef4M5Bg'
+        },
+        {
+          'Youtbe Channel': 'IQM Quantum computers',
+          URL: 'https://www.youtube.com/@IQMQuantumComputers'
+        },
+        {
+          'Youtbe Channel': 'Quantum Computing',
+          URL: 'https://www.youtube.com/@quantumcomputing957'
+        },
+        {
+          'Youtbe Channel': 'Michael Nielsen',
+          URL: 'https://www.youtube.com/@MichaelNielsen'
+        },
+        {
+          'Youtbe Channel': 'IBM Research',
+          URL: 'https://www.youtube.com/@ibmresearch'
+        },
+        {
+          'Youtbe Channel': 'Google Quantum AI ',
+          URL: 'https://www.youtube.com/@GoogleQuantumAI'
+        },
+        {
+          'Youtbe Channel': 'D-Wave',
+          URL: 'https://www.youtube.com/@dwavequantum'
+        },
+        {
+          'Youtbe Channel': 'Cambridge Quantum',
+          URL: 'https://www.youtube.com/@CambridgeQuantumComputing'
+        },
+        { 'Youtbe Channel': 'Xanadu', URL: 'https://youtube.com/@XanaduAI' },
+        {
+          'Youtbe Channel': 'NYU Quantum Technology Lab',
+          URL: 'https://www.youtube.com/@NYUQuantumTechnologyLab'
+        },
+        {
+          'Youtbe Channel': 'Ahmed Younes',
+          URL: 'https://www.youtube.com/@drayounes'
+        },
+        {
+          'Youtbe Channel': 'Quantum Steve',
+          URL: 'https://www.youtube.com/@QuantumSteve'
+        },
+        {
+          'Youtbe Channel': 'Anastasia Marchenkova',
+          URL: 'https://www.youtube.com/@AnastasiaMarchenkovaQuantum'
+        },
+        {
+          'Youtbe Channel': 'QuantumComputing India',
+          URL: 'https://www.youtube.com/@quantumcomputingindia'
+        },
+        {
+          'Youtbe Channel': 'Sabine Hossenfelder ',
+          URL: 'https://www.youtube.com/@SabineHossenfelder'
+        },
+        {
+          'Youtbe Channel': 'Rigetti Computing',
+          URL: 'https://www.youtube.com/@rigetti'
+        },
+        {
+          'Youtbe Channel': 'Classiq Technologies',
+          URL: 'https://www.youtube.com/@ClassiqTechnologies'
+        },
+        {
+          'Youtbe Channel': 'CQC2T | Centre for Quantum Computation and Communication Technology',
+          URL: 'https://www.youtube.com/@cqc2tcentreforquantumcompu21'
+        },
+        {
+          'Youtbe Channel': 'QC Ware',
+          URL: 'https://www.youtube.com/@QCWare/about'
+        },
+        {
+          'Youtbe Channel': 'Quantum Flagship',
+          URL: 'https://www.youtube.com/@QuantumFlagship'
+        },
+        {
+          'Youtbe Channel': 'Institute for Quantum Computing',
+          URL: 'https://www.youtube.com/@QuantumIQC'
+        },
+        {
+          'Youtbe Channel': 'Quantum Computing',
+          URL: 'https://www.youtube.com/@quantumcomputing2985'
+        },
+        {
+          'Youtbe Channel': 'Horizon Quantum Computing',
+          URL: 'https://www.youtube.com/@HorizonQuantumComputing'
+        },
+        {
+          'Youtbe Channel': 'Quantum Center ETH Zurich',
+          URL: 'https://www.youtube.com/@QuantumCenterETHZurich'
+        }
+      ]
+  }
+];
+
+export default YoutubeData;
