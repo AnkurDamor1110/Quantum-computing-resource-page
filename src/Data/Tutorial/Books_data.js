b466a416b5fc01c8fa90e0f943305df5f4a1dc4c
Dhruvin2910 <dhruvinpatel321@gmail.com>
2023-11-09 09:22:52 +0530

    books_data.js updated

--- src/Data/Tutorial/Books_data.js
+++ src/Data/Tutorial/Books_data.js
@@ -40,6 +40,66 @@ const bookData =[
     author: 'Arthur O. Pittenger',
     link: 'https://books.google.co.in/books?hl=en&lr=&id=kskFCAAAQBAJ&oi=fnd&pg=PR11&dq=quantum+computing+algorithms&ots=BO151QORXr&sig=ttLXDfLgEOkGmoSAOtvljTojaoE&redir_esc=y#v=onepage&q=quantum%20computing%20algorithms&f=false',
   },
+  {
+    title: 'Quantum Computing: A Gentle Introduction',
+    author: 'Eleanor Rieffel and Wolfgang Polak',
+    link: 'https://mitpress.mit.edu/9780262526678/quantum-computing/'
+  },
+  {
+    title: 'Quantum Mechanics and Path Integrals',
+    author: 'Richard P. Feynman and Albert R. Hibbs',
+    link: 'https://www.amazon.in/Quantum-mechanics-integrals-Richard-Feynman/dp/0486477223'
+  },
+  {
+    title: 'Quantum Computing: A Short Course from Theory to Experiment',
+    author: 'Joachim Stolze and Dieter Suter',
+    link: 'https://www.wiley.com/en-us/Quantum+Computing%3A+A+Short+Course+from+Theory+to+Experiment-p-9783527617777'
+  },
+  {
+    title: 'Quantum Computing: From Linear Algebra to Physical Realizations',
+    author: 'Mikio Nakahara and Tetsuo Ohmi',
+    link: 'https://www.amazon.in/Quantum-Computing-Algebra-Physical-Realizations/dp/0750309830#:~:text=Covering%20both%20theory%20and%20progressive,computational%20power%20in%20quantum%20computing.'
+  },
+  {
+    title: 'Quantum Computing Explained',
+    author: 'David McMahon',
+    link: 'https://www.amazon.in/Quantum-Computing-Explained-Wiley-IEEE/dp/0470096993'
+  },
+  {
+    title: 'Quantum Computing since Democritus',
+    author: 'Scott Aaronson',
+    link: 'https://www.amazon.in/Quantum-Computing-Explained-Wiley-IEEE/dp/0470096993'
+  },
+  {
+    title: 'Quantum Computing: An Applied Approach',
+    author: 'Jack D. Hidary',
+    link: 'https://www.amazon.in/Quantum-Computing-Approach-Jack-Hidary/dp/3030239217'
+  },
+  {
+    title: 'Classical and Quantum Computation',
+    author: 'A. Yu. Kitaev, A. H. Shen, and M. N. Vyalyi',
+    link: 'https://www.amazon.com/Classical-Quantum-Computation-Graduate-Mathematics/dp/0821832298'
+  },
+  {
+    title: 'Quantum Computing and Quantum Information',
+    author: 'Michael Nielsen and Isaac Chuang',
+    link: 'https://www.amazon.com/Quantum-Computation-Information-10th-Anniversary/dp/1107002176'
+  },
+  {
+    title: 'An Introduction to Quantum Computing',
+    author: 'Phillip Kaye, Raymond Laflamme, and Michele Mosca',
+    link: 'https://www.amazon.com/Introduction-Quantum-Computing-Materials-Research/dp/3030693171'
+  },
+  {
+    title: 'Quantum Computer Science',
+    author: 'N. David Mermin',
+    link: 'https://www.amazon.com/Quantum-Computation-Information-10th-Anniversary/dp/1107002176'
+  },
+  {
+    title: 'Programming Quantum Computers: Essential Algorithms and Code Samples',
+    author: 'Mercedes Gimeno-Segovia',
+    link: 'https://www.amazon.com/Programming-Quantum-Computers-Essential-Algorithms/dp/1492039683'
+  },
 ];
   
   export default bookData;
