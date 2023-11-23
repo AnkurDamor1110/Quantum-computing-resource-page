5d8c2d4c61fcff51567e55ad77425f10abc76a12
Ankur Damor <ankurdamor1110@gmail.com>
2023-11-23 13:47:50 +0530

    disclaimer added

--- src/Components/pages/Footer/Footer.js
+++ src/Components/pages/Footer/Footer.js
@@ -17,11 +17,11 @@ const Footer = () => {
           <div className='box link'>
             <h3>Team</h3>
             <ul>
-              <li>Ankur Dmaor</li>
-              <li>Dhuvin Patel</li>
-              <li>Monil Rathod</li>
-              <li>Hardik Makwana</li>
-              <li>Prince Patil</li>
+              <li><a href="https://www.linkedin.com/in/ankurdamor/">Ankur Damor</a></li>
+              <li><a href="https://www.linkedin.com/in/dhruvin-patel-799a182a0/">Dhruvin Patel</a></li>
+              <li><a href="#">Monil Rathod</a></li>
+              <li><a href="#">Hardik Makwana</a></li>
+              <li><a href="#">Prince Patil</a></li>
             </ul>
           </div>
           <div className='box link'>
@@ -35,14 +35,11 @@ const Footer = () => {
           </div>
          
           <div className='box last'>
-            <h3>Have a Questions?</h3>
+            <h3>Disclaimer</h3>
             <ul>
             <li>
-                <i className='fas fa-map-marker'></i>
-                <a href='https://www.google.com/maps/place/Gupta+Lab/@23.1893849,72.628436,15z/data=!4m6!3m5!1s0x395c2a3c98817075:0x504d15432808513a!8m2!3d23.1893849!4d72.628436!16s%2Fg%2F11j8gy40w4?entry=ttu' target='_blank' rel='noopener noreferrer'>
-                  4, Faculty Block 2, Room 2212, Post Bag, Daiict college Near, Indroda Cir, Gandhinagar, Gujarat 382007
-                
-                </a>
+              <i>&#x26A1;</i>   {/*icon has to be added*/}
+                <p className="Disclaimer">This is an ongoing project, and the website currently lacks complete data. If you have any suggestions for adding data, please email us at the address below.</p>
               </li>
               <li>
               <i className='fas fa-paper-plane'></i>
